706eae1b2ee7075ff23d5cd51efc887743c1271d
Sergo58 <godfather59@yandex.ru>
2020-11-22 20:56:36 +0500

    Updates

---
+++ precache-manifest.43287b4b3c1e2e88717b7949497ecf05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29553cfa011d009d89a646df2f11b287",
+    "url": "/HW/index.html"
+  },
+  {
+    "revision": "1f631c9d4c6546c3d468",
+    "url": "/HW/static/css/main.4ddfddd7.chunk.css"
+  },
+  {
+    "revision": "6636f3052620e5a51991",
+    "url": "/HW/static/js/2.b1758266.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW/static/js/2.b1758266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f631c9d4c6546c3d468",
+    "url": "/HW/static/js/main.6d7fe406.chunk.js"
+  },
+  {
+    "revision": "f4e08d7fea8dae7a3704",
+    "url": "/HW/static/js/runtime-main.9e739d5d.js"
+  },
+  {
+    "revision": "b702be85581f5bb0ad78305bc01f7acf",
+    "url": "/HW/static/media/klipartz.com.b702be85.png"
+  }
+]);
